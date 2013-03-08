c56a34236a846a88d547bbcddef2e2281760f4e6
Sergey <voronin100@gmail.com>
2013-03-08 13:36:31 +0400

    add smiles and theme change

--- js/jquery.emotions.js
+++ js/jquery.emotions.js
@@ -7,6 +7,9 @@
  * */
 
 (function($){ 
+  function escapeRegExp(s) {
+    return s.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&')
+  }
   $.fn.emotions = function(options){
     $this = $(this);
     var opts = $.extend({}, $.fn.emotions.defaults, options);
@@ -18,22 +21,24 @@
       for(i=0; i<o.s.length; i++){
 	encoded[i] = String(o.s[i]).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
       }
+	console.log(encoded);
       for(j=0; j<o.s.length; j++){
 	var repls = x.html();
+	console.log(repls);
 	if(repls.indexOf(o.s[j]) || repls.indexOf(encoded[j])){
 	  var imgr = o.a+o.b[j]+"."+o.c;      
 	  var rstr = "<img src='"+imgr+"' border='0' />"; 
-	  x.html(repls.replace(o.s[j],rstr));
-	  x.html(repls.replace(encoded[j],rstr));
+	  x.html(repls.replace(new RegExp(escapeRegExp(o.s[j]), "g"),rstr));
+	  x.html(repls.replace(new RegExp(escapeRegExp(encoded[j]), "g"),rstr));
 	}
-      }
+	}
     });
   } 
   // Defaults
   $.fn.emotions.defaults = {
     a : "emotions-fb/",	    // Emotions folder
     b : new Array("angel","colonthree","confused","cry","devil","frown","gasp","glasses","grin","grumpy","heart","kiki","kiss","pacman","smile","squint","sunglasses","tongue","unsure","upset","wink"),	  // Emotions Type
-    s : new Array("o:)",":3","o.O",":'(","3:)",":(",":O","8)",":D",">:(","<3","^_^",":*",":v",":)","-_-","8|",":p",":/",">:O",";)"),
+    s : new Array("o\:\)",":3","o.O",":'\(","3:\)",":\(",":O","8\)",":D",">:\(","<3","^_^",":*",":v",":\)","-_-","8\|",":p",":\/",">:O",";\)"),
     c : "gif"	  // Emotions Image format
   };
 })(jQuery);
